7b4abab52febe5b467cb505cecb2682669bd1c00
Bhuvan Sood <bhuvanroxx786@gmail.com>
2023-11-02 23:00:59 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ArtServicesService } from 'src/shared/services/art-services.service';
+import { Art } from 'src/models/model.model';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit, AfterViewInit{
+  title = 'ArtworkLibrary';
+  isActive = '';
+  arts: Art[] = [];
+  
+  constructor(private artservice: ArtServicesService) {
+    this.arts = this.artservice.allArts;  
+    console.log(this.arts);
+  }
+  ngOnInit(){
+    this.artservice.getData(0,12);
+  }
+  ngAfterViewInit(){
+    console.log(this.arts);
+  }
+}
+
+
